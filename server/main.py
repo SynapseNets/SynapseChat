1e1651b925638d2be070fbb59467c7d7fcb1d259
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-06-18 22:38:16 +0200

    added some docs and recieve/send all functions

--- server/main.py
+++ server/main.py
@@ -1,13 +1,17 @@
 from utils import db
+from utils import receiver
 import asyncio
 
-async def handler(reader, writer):
-    pass
+async def handler(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    payload = await receiver.receive_all(reader)
+    # use payload
 
 async def start_server():
     loop = asyncio.get_event_loop()
     await db.init_db(loop)
     server = await asyncio.start_server(handler, '0.0.0.0', port=5050)
+    async with server:
+        await server.serve_forever()
 
 async def close_server(server: asyncio.Server):
     server.close()
