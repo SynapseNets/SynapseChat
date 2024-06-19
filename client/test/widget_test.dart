8ea2658593818164c23cca59b176bbbc3bd7f6ad
Mattia Cincotta <mattia.cinco@gmail.com>
2024-06-19 23:29:22 +0200

    removed useless test

--- client/test/widget_test.dart
+++ client/test/widget_test.dart
@@ -12,20 +12,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:client/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const SynapseNetsApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  
 }
