e385568cd85c8157e10a421e40b73ef179028c6b
Mattia Cincotta <mattia.cinco@gmail.com>
2024-06-21 10:30:20 +0200

    fix asset

--- client/lib/settingspage.dart
+++ client/lib/settingspage.dart
@@ -11,17 +11,40 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  File?_image;
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickImage() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null){setState(() {
+      _image = File(image.path);
+    });}
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
-    /*
     return Scaffold(
       appBar: AppBar(
         title: const Text('Settings'),
       ),
       body: Center(
-        child:  ,),
-    )
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius:  80,
+              backgroundImage: _image == null
+              ? const AssetImage('assets/default_profile.png')
+              : FileImage(_image!) as ImageProvider, 
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _pickImage,
+              child: const Text('Change profile Picture'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-  */
-}}
+}
