0a5f79121c9be4e5ca834a76cae1eb562d0f5fc2
Mattia Cincotta <mattia.cinco@gmail.com>
2024-06-21 10:15:21 +0200

    changed dependencies

--- client/lib/settingspage.dart
+++ client/lib/settingspage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -10,6 +13,15 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
+    /*
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: Center(
+        child:  ,),
+    )
   }
-}
+  */
+}}
