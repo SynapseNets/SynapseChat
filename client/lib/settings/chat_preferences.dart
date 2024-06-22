bbed88cb70ef424efdc2fba38e52e454ab6b439a
Mattia Cincotta <mattia.cinco@gmail.com>
2024-06-22 21:44:04 +0200

    improved chat preferences page

--- client/lib/settings/chat_preferences.dart
+++ client/lib/settings/chat_preferences.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class ChatPreferencesPage extends StatefulWidget {
   const ChatPreferencesPage({super.key});
@@ -9,60 +10,101 @@ class ChatPreferencesPage extends StatefulWidget {
 
 class _ChatPreferencesPageState extends State<ChatPreferencesPage> {
   double sizeChar = 15;
+  Color backgroundColor = const Color.fromARGB(255, 134, 120, 235);
+  bool isDarkTheme = false;
+
+  void toggleTheme(bool value) {
+    setState(() {
+      isDarkTheme = value;
+      if (isDarkTheme) {
+        lightThemeSelected = false;
+        darkThemeSelected = true;
+      } else {
+        lightThemeSelected = true;
+        darkThemeSelected = false;
+      }
+    });
+  }
+
+  bool lightThemeSelected = true;
+  bool darkThemeSelected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize theme based on system theme
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      var brightness = MediaQuery.of(context).platformBrightness;
+      setState(() {
+        isDarkTheme = brightness == Brightness.dark;
+        if (isDarkTheme) {
+          lightThemeSelected = false;
+          darkThemeSelected = true;
+        } else {
+          lightThemeSelected = true;
+          darkThemeSelected = false;
+        }
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Chat Settings'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return MaterialApp(
+      theme: isDarkTheme ? ThemeData.dark() : ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Chat Settings'),
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(
-                  'Font Size',
-                  style: TextStyle(fontSize: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Font Size',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    Text(
+                      sizeChar.toInt().toString(),
+                      style: const TextStyle(fontSize: 25),
+                    ),
+                  ],
                 ),
-                Text(
-                  sizeChar.toInt().toString(),
-                  style: const TextStyle(fontSize: 25),
+                Slider(
+                  value: sizeChar,
+                  min: 15,
+                  max: 30,
+                  divisions: 15,
+                  label: sizeChar.toInt().toString(),
+                  onChanged: (double value) {
+                    setState(() {
+                      sizeChar = value;
+                    });
+                  },
                 ),
-              ],
-            ),
-            Slider(
-              value: sizeChar,
-              min: 15,
-              max: 30,
-              divisions: 15,
-              label: sizeChar.toInt().toString(),
-              onChanged: (double value) {
-                setState(() {
-                  sizeChar = value;
-                });
-              },
-            ),
-            const SizedBox(height: 20),
-            Flexible(
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(
-                  maxWidth: 1000,
-                  maxHeight: 250, // Imposta l'altezza massima desiderata
-                ),
-                child: Container(
+                const SizedBox(height: 20),
+                Container(
                   decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 134, 120, 235), // Colore di sfondo all'interno del contorno
+                    color: backgroundColor,
                     border: Border.all(
                       color: Colors.grey,
-                      width: 3, // Spessore del contorno
+                      width: 3,
                     ),
                     borderRadius: BorderRadius.circular(10),
                   ),
                   child: ListView(
+                    shrinkWrap: true,
                     padding: const EdgeInsets.all(10),
                     children: [
                       Align(
@@ -113,9 +155,67 @@ class _ChatPreferencesPageState extends State<ChatPreferencesPage> {
                     ],
                   ),
                 ),
-              ),
+                const SizedBox(height: 30),
+                InkWell(
+                  onTap: () {
+                    // Open a dialog to change the background color
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: const Text('Choose Background Color'),
+                          content: SingleChildScrollView(
+                            child: BlockPicker(
+                              pickerColor: backgroundColor,
+                              onColorChanged: (Color color) {
+                                setState(() {
+                                  backgroundColor = color;
+                                });
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        'images/background.png',
+                        width: 40,
+                        height: 40,
+                      ),
+                      const SizedBox(width: 10),
+                      const Text(
+                        'Change Background Color',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                CheckboxListTile(
+                  title: const Text('Light Theme'),
+                  value: lightThemeSelected,
+                  onChanged: (bool? value) {
+                    if (value != null && value) {
+                      toggleTheme(false);
+                    }
+                  },
+                ),
+                CheckboxListTile(
+                  title: const Text('Dark Theme'),
+                  value: darkThemeSelected,
+                  onChanged: (bool? value) {
+                    if (value != null && value) {
+                      toggleTheme(true);
+                    }
+                  },
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
