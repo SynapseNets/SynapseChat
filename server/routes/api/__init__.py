91721fb05c9df160e844b9e682ee340e94610e4f
Alan Davide Bovo <alandavide.bovo.stud@ispascalcomandini.it>
2024-07-21 21:24:28 +0200

    fixed bugs and added port customization

--- server/routes/api/__init__.py
+++ server/routes/api/__init__.py
@@ -9,7 +9,7 @@ from . import register
 apibp = Blueprint('api', __name__)
 
 apibp.route('/create_group', methods=['POST'])(token_required(create_group.create_group))
-apibp.route('/get_groups', methods=['POST'])(token_required(groups.get_groups))
-apibp.route('/get_messages', methods=['POST'])(token_required(messages.get_messages))
+apibp.route('/get_groups', methods=['POST'])(token_required(groups.groups))
+apibp.route('/get_messages', methods=['POST'])(token_required(messages.messages))
 apibp.route('/login', methods=['POST'])(login.login)
 apibp.route('/register', methods=['POST'])(register.register)
